d8089453444d401d977ec3fe36221539b09e1c6d
jaybon1 <ponggas2@gmail.com>
2024-12-10 00:57:26 +0900

    트랜잭셔널 어노테이션

--- src/main/java/com/example/mart/domain/product/service/ProductServiceApiV1.java
+++ src/main/java/com/example/mart/domain/product/service/ProductServiceApiV1.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ import static com.example.mart.model.product.entity.QProduct.product;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductServiceApiV1 {
 
     private final ProductRepository productRepository;
